2cca4fb7bd2b4f374f3ca226f917b732fbb96bb8
Thato Semoko <thatosmk@gmail.com>
2020-04-30 11:47:32 +0200

    adds styling on landing page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import VueObserveVisibility from 'vue-observe-visibility';
 import VueTypedJs from 'vue-typed-js';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
 
 import '@/components/layouts';
 // adding the styling files for bootstrap
@@ -13,6 +15,7 @@ Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.use(VueTypedJs);
+Vue.use(VueObserveVisibility);
 
 new Vue({
   el: '#app',
